5c4902aa3a8157aaf953611e4714b20e3444b4ce
Andrew Dieu <45775577+andrewdieu@users.noreply.github.com>
2022-08-09 00:16:42 -0500

    Create ping.ts

---
+++ commands/ping.ts
@@ -0,0 +1,16 @@
+import { ICommand } from "wokcommands";
+
+export default {
+    category: 'Testing',
+    description: 'Replies with pong',
+
+    slash: 'both',
+    testOnly: true,
+
+    callback: ({}) => {
+        return 'Pong'
+    },
+} as ICommand
+
+// !ping
+// /ping
